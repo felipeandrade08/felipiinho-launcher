3b49c3a6a19220c9165d8e47ecad45a845330866
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-08-29 03:51:05 -0400

    feat: aplicar limite de reboques por plano

--- backend/src/routes/reboques.routes.js
+++ backend/src/routes/reboques.routes.js
@@ -5,11 +5,11 @@
 const express = require('express');
 const router = express.Router();
 const ReboqueController = require('../controllers/ReboqueController');
+const { exigirLimite } = require('../middlewares/limitePlano');
 
 router.get('/', ReboqueController.listar);
 router.get('/:id', ReboqueController.buscarPorId);
-router.post('/', ReboqueController.criar);
+router.post('/', exigirLimite('reboques', 'reboques'), ReboqueController.criar);
 router.put('/:id', ReboqueController.atualizar);
 router.delete('/:id', ReboqueController.excluir);
-
 module.exports = router;
