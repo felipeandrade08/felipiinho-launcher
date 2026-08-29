5f94eb24cd893e5c0ce14cd045cbb111f42dfc3f
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-08-29 03:50:58 -0400

    feat: aplicar limite de caminhoes por plano

--- backend/src/routes/caminhoes.routes.js
+++ backend/src/routes/caminhoes.routes.js
@@ -5,12 +5,12 @@
 const express = require('express');
 const router = express.Router();
 const CaminhaoController = require('../controllers/CaminhaoController');
+const { exigirLimite } = require('../middlewares/limitePlano');
 
 router.get('/', CaminhaoController.listar);
 router.get('/:id', CaminhaoController.buscarPorId);
-router.post('/', CaminhaoController.criar);
+router.post('/', exigirLimite('caminhoes', 'caminhoes'), CaminhaoController.criar);
 router.put('/:id', CaminhaoController.atualizar);
 router.delete('/:id', CaminhaoController.excluir);
-
 router.get('/alertas-consumo', CaminhaoController.alertasConsumo);
 module.exports = router;
