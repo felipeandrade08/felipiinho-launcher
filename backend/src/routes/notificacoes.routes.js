317180354b6402b4d75a2a55fe3bcbafcb2b7b2b
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-08-31 19:29:48 -0300

    fix: proteger rotas de notificacoes com autenticacao

--- backend/src/routes/notificacoes.routes.js
+++ backend/src/routes/notificacoes.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 const { NotificacaoController } = require('../controllers/NotificacaoController');
+const { exigirAutenticacao } = require('../middlewares/autenticacao');
+
+router.use(exigirAutenticacao);
 
 router.get('/stream',    NotificacaoController.stream);
 router.get('/pendentes', NotificacaoController.contarPendentes);
