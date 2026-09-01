0439ab578e3d8b376b06dee8326300c1fdd6ad22
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-01 01:12:58 -0300

    fix: padroniza autenticação e contexto de conta nas rotas

--- backend/src/routes/reboques.routes.js
+++ backend/src/routes/reboques.routes.js
@@ -5,11 +5,16 @@
 const express = require('express');
 const router = express.Router();
 const ReboqueController = require('../controllers/ReboqueController');
+const { exigirAutenticacao, carregarConta } = require('../middlewares/autenticacao');
 const { exigirLimite } = require('../middlewares/limitePlano');
+
+router.use(exigirAutenticacao);
+router.use(carregarConta);
 
 router.get('/', ReboqueController.listar);
 router.get('/:id', ReboqueController.buscarPorId);
 router.post('/', exigirLimite('reboques', 'reboques'), ReboqueController.criar);
 router.put('/:id', ReboqueController.atualizar);
 router.delete('/:id', ReboqueController.excluir);
+
 module.exports = router;
