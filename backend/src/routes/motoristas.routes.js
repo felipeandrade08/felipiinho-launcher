7ce4d5f236b53baa18cc48a519db3a7295f275c3
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-01 01:07:19 -0300

    fix: permite listar motoristas autenticados da própria conta

--- backend/src/routes/motoristas.routes.js
+++ backend/src/routes/motoristas.routes.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 const MotoristaController = require('../controllers/MotoristaController');
 const RankingController = require('../controllers/RankingController');
-const { exigirAdmin } = require('../middlewares/autenticacao');
+const { exigirAutenticacao, carregarConta, exigirAdmin } = require('../middlewares/autenticacao');
 const { exigirLimite } = require('../middlewares/limitePlano');
 
 router.get('/hall-da-fama', RankingController.hallDaFama);
@@ -15,11 +15,16 @@ router.get('/ranking/:id/eventos', RankingController.historicoEventos);
 router.get('/ranking/:id/evolucao', RankingController.evolucaoMotorista);
 router.get('/ranking/evolucao-geral', RankingController.evolucaoGeral);
 router.get('/ranking/:id/multas', RankingController.multasMotorista);
+
+router.use(exigirAutenticacao);
+router.use(carregarConta);
+
+// A listagem é necessária para selects administrativos (Caminhões e Abastecimentos).
+router.get('/', MotoristaController.listar);
 router.get('/:id', MotoristaController.buscarPorId);
 
 router.delete('/ranking', exigirAdmin, RankingController.zerarRanking);
 router.delete('/ranking/:id', exigirAdmin, RankingController.zerarRankingMotorista);
-router.get('/', exigirAdmin, MotoristaController.listar);
 router.post('/', exigirAdmin, exigirLimite('motoristas', 'motoristas'), MotoristaController.criar);
 router.put('/:id', exigirAdmin, MotoristaController.atualizar);
 router.delete('/:id', exigirAdmin, MotoristaController.excluir);
