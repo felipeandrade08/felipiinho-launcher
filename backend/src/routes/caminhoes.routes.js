46947905c7a75248e6a37556d29060908dcfe5dd
Felipe Andrade <felipe.pessoall2026@gmail.com>
2026-09-01 01:07:16 -0300

    fix: autentica e carrega conta nas rotas de caminhões

--- backend/src/routes/caminhoes.routes.js
+++ backend/src/routes/caminhoes.routes.js
@@ -5,12 +5,17 @@
 const express = require('express');
 const router = express.Router();
 const CaminhaoController = require('../controllers/CaminhaoController');
+const { exigirAutenticacao, carregarConta } = require('../middlewares/autenticacao');
 const { exigirLimite } = require('../middlewares/limitePlano');
 
+router.use(exigirAutenticacao);
+router.use(carregarConta);
+
 router.get('/', CaminhaoController.listar);
+router.get('/alertas-consumo', CaminhaoController.alertasConsumo);
 router.get('/:id', CaminhaoController.buscarPorId);
 router.post('/', exigirLimite('caminhoes', 'caminhoes'), CaminhaoController.criar);
 router.put('/:id', CaminhaoController.atualizar);
 router.delete('/:id', CaminhaoController.excluir);
-router.get('/alertas-consumo', CaminhaoController.alertasConsumo);
+
 module.exports = router;
